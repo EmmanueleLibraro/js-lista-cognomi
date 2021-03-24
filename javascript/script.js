b0db2a0fa14a9a6bb3131004f1c4ea9af1fe36f0
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-24 16:06:40 +0100

    lista

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,16 @@
 // 2. inseriscilo in un array con altri cognomi: ‘bianchi’, ‘neri’, ‘rossi’, ‘verdi’, ‘gialli’
 // 3. stampa la lista ordinata alfabeticamente
 // 4. scrivi anche la posizione "umana", 1 oppure 2 oppure 3 oppure ecc... (non 0 oppure 1 oppure 2 oppure ecc.. tipici degli array), del nuovo utente inserito sopra all'interno dell'array, dopo l'ordinamento alfabetico
+
+
+var userName = parseInt ( prompt("Inserisci nome") );
+
+var lista = ["bianchi", "neri", "rossi", "verdi", "gialli"];
+console.log(lista);
+
+
+for (var i = 0; i < lista.length; i++){
+    lista.push('marroni');
+    break;
+}
+console.log(lista[i]);
